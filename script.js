96fe4fa3ed27310ca0b2d83c5f4eae3fb1188267
Jonathan Quezada <jonathanquezada@MacBook-Pro-de-Jonathan.local>
2023-04-29 15:59:33 -0400

    Se crea modal para ver los proyectos

--- script.js
+++ script.js
@@ -46,3 +46,59 @@ function setActiveLink() {
 }
 
 window.addEventListener('scroll', setActiveLink);
+
+const modalButtons = document.querySelectorAll('.modal-button');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modalImage = document.querySelector('.modal-image');
+const modalTitle = document.querySelector('.modal-title');
+const modalDescription = document.querySelector('.modal-description');
+const modalClose = document.querySelector('.modal-close');
+
+// Create an array of objects to store the data for each modal
+const modals = [
+  {
+    image: 'img/web_responsive2.jpeg',
+    title: 'Desarrollo Web',
+    description: 'Crea la página web de tus sueños, el desarrollo responsivo te permite utilizar tu Aplicación Web en todos los dispositivos'
+  },
+  {
+    image: 'img/desarollo_movil.jpeg',
+    title: 'Desarrollo Móvil',
+    description: 'Crea aplicaciónes nativas para Android, IOS o multiplataforma'
+  },
+  {
+    image: 'img/data_science.jpeg',
+    title: 'Inteligencia Artificial, Machine Learning y Big Data',
+    description: 'Crea modelos predictivos, análisis de datos, Dashboard'
+  },
+  {
+    image: 'img/fotografia.jpeg',
+    title: 'Fotografía',
+    description: 'Fotografía de paisajes, bodas, bautizos, etc.'
+  }
+];
+
+modalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Get the index of the clicked button
+    const index = button.getAttribute('data-index');
+
+    // Get the corresponding data from the modals array
+    const { image, title, description } = modals[index];
+
+    // Set the image, title, and description in the modal
+    modalImage.setAttribute('src', image);
+    modalTitle.innerText = title;
+    modalDescription.innerText = description;
+
+    // Show the modal overlay
+    modalOverlay.style.display = 'block';
+  });
+});
+
+modalClose.addEventListener('click', () => {
+  // Hide the modal overlay
+  modalOverlay.style.display = 'none';
+});
+
+
